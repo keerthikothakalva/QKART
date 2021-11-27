ced4601b393d97dbc8d813a06aba0f28f743f4ee
Nabhan <nabhan@criodo.com>
2021-11-27 10:59:13 +0550

    updated changes - 2021-11-27 10:39:13 (Crio.Do)

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -18,17 +18,292 @@ import "./Checkout.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
+// Definition of Data Structures used
+/**
+ * @typedef {Object} Product - Data on product available to buy
+ *
+ * @property {string} name - The name or title of the product
+ * @property {string} category - The category that the product belongs to
+ * @property {number} cost - The price to buy the product
+ * @property {number} rating - The aggregate rating of the product (integer out of five)
+ * @property {string} image - Contains URL for the product image
+ * @property {string} _id - Unique ID for the product
+ */
+
+/**
+ * @typedef {Object} CartItem -  - Data on product added to cart
+ *
+ * @property {string} name - The name or title of the product in cart
+ * @property {string} qty - The quantity of product added to cart
+ * @property {string} category - The category that the product belongs to
+ * @property {number} cost - The price to buy the product
+ * @property {number} rating - The aggregate rating of the product (integer out of five)
+ * @property {string} image - Contains URL for the product image
+ * @property {string} productId - Unique ID for the product
+ */
+
+/**
+ * @typedef {Object} Address - Data on added address
+ *
+ * @property {string} _id - Unique ID for the address
+ * @property {string} address - Full address string
+ */
+
+/**
+ * @typedef {Object} Addresses - Data on all added addresses
+ *
+ * @property {Array.<Address>} all - Data on all added addresses
+ * @property {string} selected - Id of the currently selected address
+ */
+
+/**
+ * @typedef {Object} NewAddress - Data on the new address being typed
+ *
+ * @property { Boolean } isAddingNewAddress - If a new address is being added
+ * @property { String} value - Latest value of the address being typed
+ */
+
+/**
+ * Returns the complete data on all products in cartData by searching in productsData
+ *
+ * @param { String } token
+ *    Login token
+ *
+ * @param { NewAddress } newAddress
+ *    Data on new address being added
+ *
+ * @param { Function } handleNewAddress
+ *    Handler function to set the new address field to the latest typed value
+ *
+ * @param { Function } addAddress
+ *    Handler function to make an API call to add the new address
+ *
+ * @returns { JSX.Element }
+ *    JSX for the Add new address view
+ *
+ */
+const AddNewAddressView = ({
+  token,
+  newAddress,
+  handleNewAddress,
+  addAddress,
+}) => {
+  return (
+    <Box display="flex" flexDirection="column">
+      <TextField
+        multiline
+        minRows={4}
+        placeholder="Enter your complete address"
+      />
+      <Stack direction="row" my="1rem">
+        <Button
+          variant="contained"
+        >
+          Add
+        </Button>
+        <Button
+          variant="text"
+        >
+          Cancel
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
 
 const Checkout = () => {
+  const token = localStorage.getItem("token");
+  const history = useHistory();
+  const { enqueueSnackbar } = useSnackbar();
+  const [items, setItems] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [addresses, setAddresses] = useState({ all: [], selected: "" });
+  const [newAddress, setNewAddress] = useState({
+    isAddingNewAddress: false,
+    value: "",
+  });
 
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(`${config.endpoint}/products`);
+
+      setProducts(response.data);
+      return response.data;
+    } catch (e) {
+      if (e.response && e.response.status === 500) {
+        enqueueSnackbar(e.response.data.message, { variant: "error" });
+        return null;
+      } else {
+        enqueueSnackbar(
+          "Could not fetch products. Check that the backend is running, reachable and returns valid JSON.",
+          {
+            variant: "error",
+          }
+        );
+      }
+    }
+  };
+
+  const fetchCart = async (token) => {
+    if (!token) return;
+    try {
+      const response = await axios.get(`${config.endpoint}/cart`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      return response.data;
+    } catch {
+      enqueueSnackbar(
+        "Could not fetch cart details. Check that the backend is running, reachable and returns valid JSON.",
+        {
+          variant: "error",
+        }
+      );
+      return null;
+    }
+  };
+
+  const getAddresses = async (token) => {
+    if (!token) return;
+
+    try {
+      const response = await axios.get(`${config.endpoint}/user/addresses`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setAddresses((currAddresses) => ({
+        ...currAddresses,
+        all: response.data,
+      }));
+      return response.data;
+    } catch {
+      enqueueSnackbar(
+        "Could not fetch addresses. Check that the backend is running, reachable and returns valid JSON.",
+        {
+          variant: "error",
+        }
+      );
+      return null;
+    }
+  };
+
+  /**
+   * Handler function to add a new address and display the latest list of addresses
+   *
+   * @param { String } token
+   *    Login token
+   *
+   * @param { NewAddress } newAddress
+   *    Data on new address being added
+   *
+   * @returns { Array.<Address> }
+   *    Latest list of addresses
+   *
+   */
+  const addAddress = async (token, newAddress) => {
+    try {
+
+    } catch (e) {
+      if (e.response) {
+        enqueueSnackbar(e.response.data.message, { variant: "error" });
+      } else {
+        enqueueSnackbar(
+          "Could not add this address. Check that the backend is running, reachable and returns valid JSON.",
+          {
+            variant: "error",
+          }
+        );
+      }
+    }
+  };
+
+  /**
+   * Handler function to delete an address from the backend and display the latest list of addresses
+   *
+   * @param { String } token
+   *    Login token
+   *
+   * @param { String } addressId
+   *    Id value of the address to be deleted
+   *
+   * @returns { Array.<Address> }
+   *    Latest list of addresses
+   *
+   */
+  const deleteAddress = async (token, addressId) => {
+    try {
+
+    } catch (e) {
+      if (e.response) {
+        enqueueSnackbar(e.response.data.message, { variant: "error" });
+      } else {
+        enqueueSnackbar(
+          "Could not delete this address. Check that the backend is running, reachable and returns valid JSON.",
+          {
+            variant: "error",
+          }
+        );
+      }
+    }
+  };
+
+  /**
+   * Return if the request validation passed. If it fails, display appropriate warning message.
+   *
+   * @param { Array.<CartItem> } items
+   *    Array of objects with complete data on products added to the cart
+   *
+   * @param { Addresses } addresses
+   *    Contains data on array of addresses and selected address id
+   *
+   * @returns { Boolean }
+   *    Whether validation passed or not
+   *
+   */
+  const validateRequest = (items, addresses) => {
+  };
+
+  /**
+   * Handler function to perform checkout operation for items added to the cart for the selected address
+   *
+   * @param { String } token
+   *    Login token
+   *
+   * @param { Array.<CartItem } items
+   *    Array of objects with complete data on products added to the cart
+   *
+   * @param { Addresses } addresses
+   *    Contains data on array of addresses and selected address id
+   *
+   * @returns { Boolean }
+   *    If checkout operation was successful
+   *
+   */
+  const performCheckout = async (token, items, addresses) => {
+  };
+
+
+
+  useEffect(() => {
+    const onLoadHandler = async () => {
+      const productsData = await getProducts();
+
+      const cartData = await fetchCart(token);
+      const cartDetails = await generateCartItemsFrom(cartData, productsData);
+      setItems(cartDetails);
+    };
+    onLoadHandler();
+  }, []);
 
   return (
     <>
       <Header />
       <Grid container>
-        <Grid
-          item
-        >
+        <Grid item xs={12} md={9}>
           <Box className="shipping-container" minHeight="100vh">
             <Typography color="#3C3C3C" variant="h4" my="1rem">
               Shipping
@@ -53,6 +328,10 @@ const Checkout = () => {
 
             <Box my="1rem">
               <Typography>Wallet</Typography>
+              <Typography>
+                Pay ${getTotalCartValue(items)} of available $
+                {localStorage.getItem("balance")}
+              </Typography>
             </Box>
 
             <Button
@@ -62,6 +341,9 @@ const Checkout = () => {
               Make Payment
             </Button>
           </Box>
+        </Grid>
+        <Grid item xs={12} md={3} bgcolor="#E9F5E1">
+          <Cart isReadOnly products={products} items={items} />
         </Grid>
       </Grid>
       <Footer />
